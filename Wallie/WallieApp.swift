73eb4f7db6c182c26601db00297fcd65c1920bdf
Juan Gabriel Borsacchi Marques <112633232+Juan-Borsacchi@users.noreply.github.com>
2026-08-14 15:03:51 -0300

    Add files via upload

---
+++ Wallie/WallieApp.swift
@@ -0,0 +1,21 @@
+//
+//  WallieApp.swift
+//  Wallie
+//
+//  Created by Juan Gabriel Borsacchi Marques on 14/08/26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct WallieApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
